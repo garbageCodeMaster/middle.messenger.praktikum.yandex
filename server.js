b484eb5eacc4c38e1148fba7bb5be7c6bffba096
garbageCodeMaster <dimonchicmilto@gmail.com>
2022-09-20 00:47:26 +0300

    routing test fix

--- server.js
+++ server.js
@@ -6,27 +6,7 @@ const PORT = 3000;
 app.use(express.static("dist/"));
 
 app.get('/', function(req, res){
-  res.redirect('/home/index.html');
-});
-
-app.get('/messenger', function(req, res){
-  res.redirect('/messenger/messenger.html');
-});
-
-app.get('/register', function(req, res){
-  res.redirect('/register/register.html');
-});
-
-app.get('/about', function(req, res){
-  res.redirect('/about/about.html');
-});
-
-app.get('/404', function(req, res){
-  res.redirect('/404/404.html');
-});
-
-app.get('/500', function(req, res){
-  res.redirect('/500/500.html');
+  res.redirect('/index.html');
 });
 
 app.listen(PORT, function () {
